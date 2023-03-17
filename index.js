4d76524832bd496023a896f858f2594abb897488
Gerald Mbuthia <geraldombuthia@gmail.com>
2023-03-17 09:52:37 +0300

    made changes to index

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.post("/", (req,res) => {
     console.log(req.body);
 })
 app.listen(port, () => {
-    console.log(`Listening on port${port} `)
+    console.log(`Listening on port ${port} `)
 })
